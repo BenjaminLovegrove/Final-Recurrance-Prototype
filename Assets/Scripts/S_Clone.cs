09f8386cea5210af2ba1a9a8629f88957af14062
BenjaminLovegrove <Benjamin_jl@Live.com>
2015-04-05 10:56:50 +1000

    Added more sounds. Clones can die.

--- Assets/Scripts/S_Clone.cs
+++ Assets/Scripts/S_Clone.cs
@@ -9,6 +9,11 @@ public class S_Clone : MonoBehaviour {
 	public Quaternion originalFacing;
 	public GameObject player;
 
+	public AudioClip getHit;
+	public AudioClip death;
+
+	private Vector3 targetDir;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
@@ -21,7 +26,7 @@ public class S_Clone : MonoBehaviour {
 		//Face player if alerted
 		alertTimer -= Time.deltaTime;
 		if (alertTimer > 0) {
-			transform.LookAt (player.transform.position);
+			transform.LookAt(player.transform.position);
 		} else {
 			transform.rotation = originalFacing;
 		}
@@ -29,20 +34,24 @@ public class S_Clone : MonoBehaviour {
 
 	//This is called when the player pressed E on this object
 	void Interact(){
-		rb.AddForce (Vector3.back * 5, ForceMode.Impulse);
+		//Be pushed/damaged
+		AudioSource.PlayClipAtPoint (getHit, transform.position);
+		rb.AddForce (this.transform.position - player.transform.position * 2, ForceMode.Impulse);
 		nearClones = Physics.SphereCastAll (transform.position, 20f, Vector3.forward, 20f);
 		foreach (RaycastHit clone in nearClones) {
 			clone.collider.SendMessage("Alert", SendMessageOptions.DontRequireReceiver);
 		}
+		Alert ();
 	}
 
-	//Alerted when a neaby clone is "Interacted"
+	//Alerted when a neaby clone is "Interacted" and when this clone is
 	void Alert(){
 		alertTimer = 5f;
 	}
 
 	//Is called when the clone enters a trigger with the "kill" script
 	void Kill(){
-
+		AudioSource.PlayClipAtPoint (death, transform.position);
+		Destroy (gameObject);
 	}
 }
--- Assets/Scripts/S_Player.cs
+++ Assets/Scripts/S_Player.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class S_Player : MonoBehaviour {
 	
 	private RaycastHit[] hits;
+	private float recentlyInteracted = 0;
+	public AudioSource whispers;
 
 	// Use this for initialization
 	void Start () {
-
+		whispers = GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
@@ -17,8 +19,23 @@ public class S_Player : MonoBehaviour {
 			hits = Physics.RaycastAll(transform.position, transform.forward, 1.5f);
 			foreach (RaycastHit hit in hits){
 				hit.collider.SendMessage ("Interact");
+
+				//Have you recently interacted with a clone? (for whispering audio)
+				if (hit.collider.tag == "Clone"){
+					recentlyInteracted = 3f;
+				}
 			}
 		}
+
+		//Player whispering audio if you have recently interacted with a clone
+		if (recentlyInteracted > 0 && !whispers.isPlaying) {
+			whispers.Play ();
+		} else if (recentlyInteracted <= 0 && whispers.isPlaying) {
+			whispers.Stop ();
+		}
+
+		//Decrement timers
+		recentlyInteracted -= Time.deltaTime;
 	}
 }
 
